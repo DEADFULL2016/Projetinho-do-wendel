0b7ee58f7de6d9b3a63dcf36a66297355f044205
DEADFULL2016 <aalvesoliveira0401@gmail.com>
2024-06-06 14:06:08 -0300

    encapsulamento

---
+++ encapsulamento/carro emcapsulado.py
@@ -0,0 +1,15 @@
+class carro_encapsulado():
+    def __init__(self, Modelo,marca,traçao,carroceria,quilometragem,combustivel,motor):
+        self.__Modelo = Modelo
+        self.__Marca = marca
+        self.__traçao = traçao
+        self.__Tipo_de_Carroceria = carroceria
+        self.__Quilometragem = quilometragem
+        self.__Tipo_de_combustível = combustivel
+        self.__Motor = motor
+
+    def Modeloset(self, Audi):
+        self.__Modelo = Audi
+
+    def Modelosget(self):
+        return self.__Modelo
